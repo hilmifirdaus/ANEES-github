230e8c1f978113ddbec11569035e2bb1d56018db
hilmi firdaus <hilmi.firdaus@gmail.com>
2020-12-26 08:18:40 +0800

    helper flow step 1. 2. 3. set condition in firestore but did not store helper details!!!) ONLY STEPS

--- js/auth.js
+++ js/auth.js
@@ -33,10 +33,23 @@ function login() {
                     var userName = doc.data().userName;
                     
                     if (userType == 'Helper') {
+
+                        //check in userProfile if false or step not exist then go to certain pageS
+                        if (doc.data().step1 == "false" || !doc.data().step1) {
+                            window.location = '/pages/requirements-helper.html';
+                        }
+                        else if (doc.data().step2 == "false" || !doc.data().step2) {
+                            window.location = '/pages/step2-1.html';
+                        }
+                        else if (doc.data().step3 == "false" || !doc.data().step3) {
+                            window.location = '/pages/step-3.html';
+                        }
+                        else {
+                            window.location = '/pages/homepage-helper.html';
+                            console.log("succesfully sign in a Helper");
+                            alert("Welcome " + userName + "!");
+                        }
                         
-                        window.location = '/pages/homepage-helper.html';
-                        console.log("succesfully sign in a Helper");
-                        alert("Welcome " + userName + "!");
                     }
                     if (userType == 'Requester') {
                         
@@ -65,6 +78,7 @@ function signUp() {
     var userPass = document.getElementById("psw").value;
     var userType = document.querySelector('input[name="userType"]:checked').value;
     var timestamp = firebase.firestore.FieldValue.serverTimestamp();
+    
 
     firebase.auth().createUserWithEmailAndPassword(userEmail, userPass)
     .then((user) => {
@@ -87,7 +101,7 @@ function signUp() {
     
                 if (userType === "Helper") {
                     //this should go to requirement page but for go to step 1
-                    window.location = '/pages/step1question.html';
+                    window.location = '/pages/requirements-helper.html';
                 }
                 if (userType === "Requester") {
                     //if requester, go to requester homepage
@@ -112,4 +126,146 @@ function signUp() {
         window.alert("Error :" + errorMessage);
     });
 
+}
+
+
+//this is for checking if helper go through the step1
+function checkStep1() {
+
+    var clickedNext = document.getElementById('next');
+    var clickedBack = document.getElementById('back');
+    //var step1, step2, step3;
+    var user = firebase.auth().currentUser;
+
+    if (user && clickedNext) {
+        var uid = user.uid;
+
+        firebase.firestore().collection("userProfile").doc(uid).set({
+            step1: "true",
+            step2: "false",
+            step3: "false"
+        }, { merge: true })
+        .then(function() {
+
+            console.log("Document successfully written!");
+            alert("your application has been uplaoded successfully!");
+            window.location = '/pages/step2-1.html';
+    
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
+    }
+    else if (user && clickedBack) {
+        window.location = '/pages/how-it-works.html';
+    }
+}
+
+//this for checking if user go throu step2
+function checkStep2(whichStep) {
+
+    var clickedNext = document.getElementById('next');
+    var clickedBack = document.getElementById('back');
+    //var step1, step2, step3;
+    var user = firebase.auth().currentUser;
+
+    if (user && clickedNext && whichStep == '2-1') {
+        window.location = '/pages/step2-2.html';
+    }
+    else if (user && clickedNext && whichStep == '2-2') {
+        var trainingdate = document.getElementById('trainingsession').value;
+        var uid = user.uid;
+        firebase.firestore().collection("userProfile").doc(uid).set({
+            training: trainingdate
+        }, {merge: true})
+        .then(function() {
+
+            console.log("Document successfully written!");
+            alert("your application has been uplaoded successfully!");
+            window.location = '/pages/step2-3.html';
+    
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
+        
+    }
+    else if (user && clickedNext && whichStep == '2-3') {
+        var uid = user.uid;
+
+        firebase.firestore().collection("userProfile").doc(uid).update({
+            step1: "true",
+            step2: "true",
+            step3: "false"
+        })
+        .then(function() {
+
+            console.log("Document successfully written!");
+            alert("your application has been uplaoded successfully!");
+            window.location = '/pages/step-3.html';
+    
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
+    }
+    else if (user && clickedBack && whichStep == 'back') {
+        window.history.back();
+    }
+}
+
+//this is for step 3
+function checkStep3(whichStep) {
+
+    var clickedNext = document.getElementById('next');
+    var clickedBack = document.getElementById('back');
+    //var step1, step2, step3;
+    var user = firebase.auth().currentUser;
+
+    if (user && clickedNext && whichStep == '3-1') {
+        window.location = '/pages/step-3-guidelines.html';
+    }
+    else if (user && clickedNext && whichStep == '3-2') {
+
+        var uid = user.uid;
+
+        firebase.firestore().collection("userProfile").doc(uid).update({
+            training: "done"
+        })
+        .then(function() {
+
+            console.log("Document successfully written!");
+            alert("your application has been uplaoded successfully!");
+            window.location = '/pages/step-3-uniqueno.html';
+    
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
+
+        
+    }
+    else if (user && clickedNext && whichStep == '3-3') {
+    
+        var uid = user.uid;
+
+        firebase.firestore().collection("userProfile").doc(uid).update({
+            step1: "true",
+            step2: "true",
+            step3: "true"
+        })
+        .then(function() {
+
+            console.log("Document successfully written!");
+            alert("your application has been uplaoded successfully!");
+            window.location = '/pages/login.html';
+    
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
+    }
+    else if (user && clickedBack && whichStep == 'back') {
+        window.history.back();
+    }
 }
